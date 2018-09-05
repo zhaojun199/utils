7a400eef1c484e4bc4c7db87c29b8e5f8674d1b4
zhaojun199 <527367156@qq.com>
2018-09-05 15:24:58 +0800

    debounce新增类型判断

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var log = console.log;
+var warn = console.warn;
 
 var utils = {};
 /**
- * 空闲控制 返回函数连续调用时，空闲时间必须大于或等于 idle，action 才会执行
+ * 空闲控制 返回函数连续调用时，空闲时间必须大于或等于 delay，action 才会执行
  * @param action {function}  请求关联函数，实际应用需要调用的函数
- * @param idle   {number}    空闲时间，单位毫秒
+ * @param delay  {number}    空闲时间，单位毫秒
  * @return 		 {function}  返回客户调用函数
  */
-utils.debounce = function(action, idle) {
+utils.debounce = function(action, delay) {
+	if (typeof action !== 'function') {
+		warn('debounce receive a param not function');
+		return action;
+	}
+	delay = + delay || 0;
 	var last;
 	return function() {
 		var ctx = this, args = arguments;
@@ -16,7 +22,7 @@ utils.debounce = function(action, idle) {
 		}
 		last = setTimeout(function() {
 			action.apply(ctx, args);
-		}, idle);
+		}, delay);
 	};
 }
 
