e3a966b1a55ac1451e2f849dcb9738e18068df63
zhaojun199 <527367156@qq.com>
2018-09-05 20:37:59 +0800

    新增logs方法，直接打印序列化的json

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 var log = console.log;
 var warn = console.warn;
+var logs = function() {
+	var args = arguments;
+	var jsonArgs = Array.prototype.map.call(args, it => {
+		return JSON.stringify(it);
+	});
+	return console.log.apply(this, jsonArgs);
+}
 
-var utils = {};
 /**
  * 空闲控制 返回函数连续调用时，空闲时间必须大于或等于 delay，action 才会执行
  * @param action {function}  请求关联函数，实际应用需要调用的函数
  * @param delay  {number}    空闲时间，单位毫秒
  * @return 		 {function}  返回客户调用函数
  */
-utils.debounce = function(action, delay) {
+function debounce(action, delay) {
 	if (typeof action !== 'function') {
 		warn('debounce receive a param not function');
 		return action;
@@ -25,5 +31,9 @@ utils.debounce = function(action, delay) {
 		}, delay);
 	};
 }
+
+var utils = {};
+utils.logs = logs;
+utils.debounce = debounce;
 
 module.exports = utils;
